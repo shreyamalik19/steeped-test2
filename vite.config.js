cf2f9f75e228e16e58dfde25fed1a884f28694ee
Shreya Malik <shreyamalik@MacBook-Air-5.local>
2025-05-29 22:23:41 -0700

    vercel build issue 2

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  root: '.',  // This ensures it looks in the right place
+  build: {
+    outDir: 'dist'
+  }
 });
+
